6bf64c7a3ef8f960bb3d873251a591470f6f2ff4
Paladign <paladign@protonmail.com>
2022-10-26 20:31:16 -0400

    Created

---
+++ Lock.lua
@@ -0,0 +1,26 @@
+local Passphrase = ""
+
+oldpull = os.pullEvent
+os.pullEvent = os.pullEventRaw
+
+local function reset()
+    shell.run("clear")
+    print("This device is password protected.")
+    write("Password> ")
+end
+reset()
+while true do
+    local typed = read("*")
+    if typed == Passphrase then
+        print("Successfully logged in.")
+        sleep(1)
+        shell.run("clear")
+        shell.run("shell")
+        os.pullEvent = oldPull
+        break
+    else
+        print("Please try again.")
+        sleep(1)
+        reset()
+    end
+end
