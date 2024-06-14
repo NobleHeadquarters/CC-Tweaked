8cf763001ebe68b21dc68d9dc21cad98a6e91095
paladign <111393518+paladign@users.noreply.github.com>
2024-06-14 17:28:35 -0400

    Update build.lua

--- NobleOS/build.lua
+++ NobleOS/build.lua
@@ -2,7 +2,7 @@ print("Are you sure you want to delete all files and install NobleOS? (Y/N)")
 
 if string.lower(read()) == "y" then
     shell.run("delete *")
-    shell.run("copy disk/xstartup.lua startup.lua")
+    shell.run("copy disk/newStartup.lua startup.lua")
     shell.run("copy disk/lock.lua lock.lua")
     shell.run("clear")
     print("Completed install!")
