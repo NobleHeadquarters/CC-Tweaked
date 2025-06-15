858248eafacf65e45175015bb49647ae4bb1f786
NobleHeadquarters <111393518+NobleHeadquarters@users.noreply.github.com>
2025-06-15 19:01:57 -0400

    Update functionFinder.lua

--- functionFinder.lua
+++ functionFinder.lua
@@ -1,4 +1,9 @@
 local p = peripheral.wrap("back")
+local monitor = peripheral.find("monitor")
+
+term.redirect(monitor)
+monitor.setTextScale(0.5)
+
 for fName, func in pairs(p) do
     print(fName)
     print(func)
