b3924f843e0a6c4819b347985d3c5091866e0af0
NobleHeadquarters <111393518+NobleHeadquarters@users.noreply.github.com>
2026-07-03 15:07:31 -0400

    Create silly scan thing.lua

---
+++ silly scan thing.lua
@@ -0,0 +1,17 @@
+local p = peripheral.find("geo_scanner")
+
+local args = ...
+
+local radius = args[1]
+
+local results = p.scan(radius)
+
+local tag = "c:ores"
+
+for i, v in pairs(results) do
+    for i2, v2 in pairs(v.tags) do
+        if v2 == tag then
+            print(v.name)
+        end
+    end
+end
