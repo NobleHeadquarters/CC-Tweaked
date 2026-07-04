f87743ccc10b3b4019745f26175e6b8024e7dc15
NobleHeadquarters <111393518+NobleHeadquarters@users.noreply.github.com>
2026-07-04 11:36:24 -0400

    Update silly scan thing.lua

--- silly scan thing.lua
+++ silly scan thing.lua
@@ -2,19 +2,35 @@ local p = peripheral.find("geo_scanner")
 
 local args = {...}
 
-local radius = 10
+local radius = 16
 
 local results = p.scan(radius)
 
-local tag = "c:ores"
+local tag = "minecraft:block/c:ores"
 
 print("cost: " .. p.cost(radius) .. "FE")
 
+local counters = {}
+
 for _, result in pairs(results) do
-    if result.name == "minecraft:coal_ore" then
-        for _, tagvalue in pairs(result.tags) do
-            print(tagvalue)
+    for _, tagvalue in pairs(result.tags) do
+        if tagvalue == tag then
+            local exists = false
+            for index, found in pairs(counters) do
+                if index == result.name then
+                    exists = true
+                    break
+                end
+            end
+            if exists == false then
+                counters[result.name] = 0
+            end
+            counters[result.name] += 1
+            break
         end
-        break
     end
+end
+
+for index, value in pairs(counters) do
+    print("Found " .. value .. " " .. index .. " within " .. radius .. " blocks")
 end
